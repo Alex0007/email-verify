d64adcd02e9351b21d001c87aa6b07387a7c72e6
Alex0007 <Alex0007lolpvp@gmail.com>
2016-02-04 21:20:47 +0500

    rebuild

--- .build/index.compiled.for.4.js
+++ .build/index.compiled.for.4.js
@@ -22,7 +22,16 @@ var verifier = {};
 
 verifier.verify = function (email, options, callback) {
   return new _promise2.default(function (resolve, reject) {
-    // Handle optional parameters
+    var calledback = false;
+    var cb = function cb(err, info) {
+      if (!calledback) {
+        calledback = true;
+        callback && callback(err, info);
+        if (err) return reject(err);
+        resolve(info);
+      }
+    };
+
     if (!email) {
       throw new Error('Missing parameters in email-verify.verify()');
     } else if (typeof options === 'function') {
@@ -39,9 +48,7 @@ verifier.verify = function (email, options, callback) {
     }, options);
 
     if (!validator.validate(email)) {
-      var response = { success: false, info: 'Invalid Email Structure', addr: email };
-      callback && callback(null, response);
-      return resolve(response);
+      return cb(null, { success: false, info: 'Invalid Email Structure', addr: email });
     }
 
     // Get the domain of the email address
@@ -62,120 +69,104 @@ verifier.verify = function (email, options, callback) {
     // Get the MX Records to find the SMTP server
     dns.resolveMx(domain, function (err, addresses) {
       if (err || typeof addresses === 'undefined') {
-        callback && callback(err, null);
-        reject(err);
+        cb(err, null);
       } else if (addresses && addresses.length <= 0) {
-        var _response = { success: false, info: 'No MX Records' };
-        callback(null, _response);
-        resolve(_response);
+        cb(null, { success: false, info: 'No MX Records' });
       } else {
-        // Find the lowest priority mail server
-        var priority = 10000;
-        var index = 0;
-        for (var i = 0; i < addresses.length; i++) {
-          if (addresses[i].priority < priority) {
-            priority = addresses[i].priority;
-            index = i;
-          }
-        }
-        var smtp = addresses[index].exchange;
-        var stage = 0;
-
-        var socket = net.createConnection(options.port, smtp);
-        var success = false;
-        var response = '';
-        var completed = false;
-        var calledback = false;
-        var ended = false;
-
-        if (options.timeout > 0) {
-          socket.setTimeout(options.timeout, function () {
-            if (!calledback) {
-              calledback = true;
-              var _response2 = { success: false, info: 'Connection Timed Out', addr: email };
-              callback && callback(null, _response2);
-              resolve(_response2);
-            }
-            socket.destroy();
-          });
-        }
-
-        socket.on('data', function (data) {
-          response += data.toString();
-          completed = response.slice(-1) === '\n';
-
-          if (completed) {
-            switch (stage) {
-              case 0:
-                if (response.indexOf('220') > -1 && !ended) {
-                  // Connection Worked
-                  socket.write('EHLO ' + options.fqdn + '\r\n', function () {
-                    stage++;
-                    response = '';
-                  });
-                } else {
-                  socket.end();
-                }
-                break;
-              case 1:
-                if (response.indexOf('250') > -1 && !ended) {
-                  // Connection Worked
-                  socket.write('MAIL FROM:<' + options.sender + '>\r\n', function () {
-                    stage++;
-                    response = '';
-                  });
-                } else {
-                  socket.end();
-                }
-                break;
-              case 2:
-                if (response.indexOf('250') > -1 && !ended) {
-                  // MAIL Worked
-                  socket.write('RCPT TO:<' + email + '>\r\n', function () {
-                    stage++;
-                    response = '';
-                  });
-                } else {
-                  socket.end();
-                }
-                break;
-              case 3:
-                if (response.indexOf('250') > -1 || options.ignore && response.indexOf(options.ignore) > -1) {
-                  // RCPT Worked
-                  success = true;
-                }
-                stage++;
-                response = '';
-                // close the connection cleanly.
-                if (!ended) socket.write('QUIT\r\n');
-                break;
-              case 4:
-                socket.end();
+        (function () {
+          // Find the lowest priority mail server
+          var priority = 10000;
+          var index = 0;
+          for (var i = 0; i < addresses.length; i++) {
+            if (addresses[i].priority < priority) {
+              priority = addresses[i].priority;
+              index = i;
             }
           }
-        }).on('connect', function (data) {}).on('error', function (err) {
-          ended = true;
-          if (!calledback) {
-            var _response3 = { success: false, info: null, addr: email };
-            calledback = true;
-            callback && callback(err, _response3);
-            resolve(_response3);
+          var smtp = addresses[index].exchange;
+          var stage = 0;
+
+          var socket = net.createConnection(options.port, smtp);
+          var success = false;
+          var response = '';
+          var completed = false;
+          var ended = false;
+
+          if (options.timeout > 0) {
+            socket.setTimeout(options.timeout, function () {
+              cb(null, { success: false, info: 'Connection Timed Out', addr: email });
+              socket.destroy();
+            });
           }
-        }).on('end', function () {
-          ended = true;
-          if (!calledback) {
-            var _response4 = {
+
+          socket.on('data', function (data) {
+            response += data.toString();
+            completed = response.slice(-1) === '\n';
+
+            if (completed) {
+              switch (stage) {
+                case 0:
+                  if (response.indexOf('220') > -1 && !ended) {
+                    // Connection Worked
+                    socket.write('EHLO ' + options.fqdn + '\r\n', function () {
+                      stage++;
+                      response = '';
+                    });
+                  } else {
+                    socket.end();
+                  }
+                  break;
+                case 1:
+                  if (response.indexOf('250') > -1 && !ended) {
+                    // Connection Worked
+                    socket.write('MAIL FROM:<' + options.sender + '>\r\n', function () {
+                      stage++;
+                      response = '';
+                    });
+                  } else {
+                    socket.end();
+                  }
+                  break;
+                case 2:
+                  if (response.indexOf('250') > -1 && !ended) {
+                    // MAIL Worked
+                    socket.write('RCPT TO:<' + email + '>\r\n', function () {
+                      stage++;
+                      response = '';
+                    });
+                  } else {
+                    socket.end();
+                  }
+                  break;
+                case 3:
+                  if (response.indexOf('250') > -1 || options.ignore && response.indexOf(options.ignore) > -1) {
+                    // RCPT Worked
+                    success = true;
+                  }
+                  stage++;
+                  response = '';
+                  // close the connection cleanly.
+                  if (!ended) socket.write('QUIT\r\n');
+                  break;
+                case 4:
+                  socket.end();
+              }
+            }
+          }).on('connect', function (data) {}).on('error', function (err) {
+            ended = true;
+            cb(err, { success: false, info: null, addr: email });
+          }).on('end', function () {
+            ended = true;
+            cb(null, {
               success: success,
               info: email + ' is ' + (success ? 'a valid' : 'an invalid') + ' address',
               addr: email
-            };
-            calledback = true;
-            callback && callback(null, _response4);
-            resolve(_response4);
-          }
-        });
+            });
+          });
+        })();
       }
     });
+
     return true;
   });
 };
